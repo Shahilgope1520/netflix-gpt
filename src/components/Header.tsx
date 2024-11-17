6e19f23d91304261a1402c32e923d1cb0961023c
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-11-17 17:01:59 +0530

    sign in and signup form + ui (#2)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
-
+import NetflixLogo from "../assets/images/Netflix_Logo_PMS.png";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="absolute w-full z-10 p-2  bg-gradient-to-b from-black to-transparent">
+      <img className="w-44" src={NetflixLogo} />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+import Header from "./Header";
 
-const Login = () => {
+const Login = ({ isSignInProp = true }) => {
+  const [isSignIn, setIsSignIn] = useState(isSignInProp);
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="absolute h-full w-full">
+        <img className="object-cover h-full w-full" src="https://assets.nflxext.com/ffe/siteui/vlv3/03ad76d1-e184-4d99-ae7d-708672fa1ac2/web/IN-en-20241111-TRIFECTA-perspective_149877ab-fcbd-4e4f-a885-8d6174a1ee81_small.jpg" />
+      </div>
+      <form className="bg-[rgba(5,6,8,0.85)] p-12 z-30 absolute w-fit lg:w-3/12 mx-auto my-36  top-0 left-0 right-0 text-white">
+        <h1 className="text-3xl font-bold leading-normal  my-2">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn && (
+          <input
+            type="text"
+            placeholder="Full name"
+            className="p-2 my-2 w-full"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email address"
+          className="p-2 my-2 w-full"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full"
+        />
+        <button className="p-2 my-2 w-full  bg-[#d9232e]">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="my-2 cursor-pointer" onClick={() => setIsSignIn((prev) => !prev)}>
+          {isSignIn
+            ? " New to Netflix? Sign Up now."
+            : "Already registered? Sign In now."}
+        </p>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
